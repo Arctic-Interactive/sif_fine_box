bbb9e8c0fa11652e310ac471a416ecc2e714c368
nikodyring <nikodyring@gmail.com>
2024-12-20 01:22:40 +0100

    Add edit profile template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   authenticated :user do
     root "dashboard#main", as: :authenticated_root
   end
